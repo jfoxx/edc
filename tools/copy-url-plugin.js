68c2e28cba8fc70f477c715698ddcd501faaf313
Jeff Foxx <foxx@adobe.com>
2025-08-09 18:31:47 -0400

    cpy url

--- tools/copy-url-plugin.js
+++ tools/copy-url-plugin.js
@@ -1,20 +1,21 @@
-window.hlx.initEditorPlugin({
-  id: 'copy-published-url',
-  label: 'Copy Published URL',
-  icon: 'copy',
-  async click({ content, config, document }) {
-    const { pathname } = document.location;
-    const { owner, repo, ref } = config;
+export default function init({ config, editor, content, display }) {
+  display.button({
+    id: 'copy-url',
+    condition: () => true,
+    text: 'Copy Published URL',
+    icon: 'copy',
+    async action() {
+      const { pathname } = window.location;
+      const { owner, repo, ref } = config;
+      const pubUrl = `https://${ref}--${repo}--${owner}.aem.page${pathname}`;
 
-    const publishedUrl = `https://${ref}--${repo}--${owner}.aem.page${pathname}`;
-
-    try {
-      await navigator.clipboard.writeText(publishedUrl);
-      console.log(`Copied: ${publishedUrl}`);
-      alert(`Copied to clipboard:\n${publishedUrl}`);
-    } catch (e) {
-      console.error('Copy failed:', e);
-      alert('Failed to copy URL to clipboard.');
-    }
-  },
-});
+      try {
+        await navigator.clipboard.writeText(pubUrl);
+        alert(`Copied to clipboard:\n${pubUrl}`);
+      } catch (err) {
+        console.error('Failed to copy URL', err);
+        alert('Failed to copy URL');
+      }
+    },
+  });
+}
